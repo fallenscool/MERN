6c6982e3bd152e191fbfbf88594964e2818d6689
fallenscool <ducelion333@gmail.com>
2020-02-24 21:31:57 +0200

    init

---
+++ routes/auth.routes.js
@@ -0,0 +1,8 @@
+const { Router } = require('express');
+const router = Router();
+
+router.post('/register', async (req, res) => { });
+
+router.post('/login', async (req, res) => { });
+
+module.exports = router;
